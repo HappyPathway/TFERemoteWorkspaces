e8b2a4cf1e9d5d52d44c9c6d535c617bd5ca83a4
Dave Arnold <darnold@hashicorp.com>
2018-11-30 11:41:41 -0800

    first push

---
+++ main.tf
@@ -0,0 +1,20 @@
+//--------------------------------------------------------------------
+// Workspace Data
+data "terraform_remote_state" "awsdemo_darnold_network" {
+  backend = "atlas"
+  config {
+    name    = "AWSDemoDarnold/Network"
+  }
+}
+
+//--------------------------------------------------------------------
+// Modules
+module "ec2_instance" {
+  source  = "app.terraform.io/AWSDemoDarnold/ec2-instance/aws"
+  version = "2.0.0"
+
+  instance_name = "DarnoldDemo"
+  instance_type = "m4.large"
+  key_name = "tfe-demos-darnold"
+  subnet_id = "${element(data.terraform_remote_state.awsdemo_darnold_network.public_subnets, 0)}"
+}
